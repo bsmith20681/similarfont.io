d4a7afd87a8d5ad458442174885ed8e7569c0f27
Ben <bsmith20681@gmail.com>
2021-02-12 09:42:47 -0700

    added mobile menu

--- components/Header.js
+++ components/Header.js
@@ -1,20 +1,37 @@
 import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 const Header = (props) => {
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const toggleMenu = (e) => setMobileMenu(!mobileMenu);
   return (
-    <div className="container">
-      <div className="row" style={{ justifyContent: "space-between" }}>
-        <Link href="/">similarfont.io</Link>
-        <ul>
-          <li>
+    <header className="container">
+      {/* Actual Menu*/}
+      <nav>
+        <h3>
+          <Link href="/">logo</Link>
+        </h3>
+        <ul className={"nav-link-list " + (mobileMenu ? "open" : "")}>
+          <li className="nav-link">
             <a href="">Home</a>
           </li>
-          <li>
+          <li className="nav-link">
             <a href="">About Us</a>
           </li>
+          <li className="nav-link">
+            <a href="">Contact Us</a>
+          </li>
         </ul>
+      </nav>
+      <div id="hamburger" onClick={toggleMenu}>
+        <div></div>
+        <div></div>
+        <div></div>
       </div>
-    </div>
+      <a href="#" id={mobileMenu ? "overlay" : ""} onClick={toggleMenu}></a>
+    </header>
   );
 };
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import "../styles/grid.css";
 import "../styles/LinkBox.css";
+import "../styles/header.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
