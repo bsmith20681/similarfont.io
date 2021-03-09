add6c984c3cc427192f6f3c883a98cdb0b692bec
Ben <bsmith20681@gmail.com>
2021-03-08 22:32:44 -0700

    added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,6 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as gtag from "../lib/gtag";
 import "../styles/globals.css";
 import "../styles/grid.css";
 import "../styles/LinkBox.css";
@@ -11,8 +14,19 @@ import "../styles/post.css";
 import "../styles/authorBox.css";
 import "../styles/bootstrap-reboot.scss";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+const App = ({ Component, pageProps }) => {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
 
-export default MyApp;
+  return <Component {...pageProps} />;
+};
+
+export default App;
